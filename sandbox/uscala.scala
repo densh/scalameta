53e6e7e584e12619176597ed07411177120e9bc0
Denys Shabalin <denys.shabalin@typesafe.com>
2014-06-24 20:11:57 +0200

    wip

--- sandbox/uscala.scala
+++ sandbox/uscala.scala
@@ -1,33 +1,39 @@
+package uscala
+
 import org.scalareflect.show.Show, Show.{ sequence => s, repeat => r, indent => i, newline => n }
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 import scala.util.parsing.input.CharSequenceReader
+import scala.language.higherKinds
 
-sealed trait Tree { override def toString = util.show(this).toString }
-  sealed trait Type extends Tree
-  object Type {
-    final case class Func(from: Type, to: Type) extends Type
-    final case class Rec(fields: List[(Name, Type)]) extends Type
-  }
-  sealed trait Stmt extends Tree
-    final case class Val(name: Name, typ: Type, value: Term) extends Stmt
-    final case class Import(t: Term, sels: List[Import.Sel]) extends Stmt
-    object Import {
-      sealed trait Sel extends Tree
-      final case class Rename(from: Name, to: Name) extends Sel
-      final case class Exclude(name: Name) extends Sel
-      final case object Wildcard extends Sel
+trait TreeTemplate {
+  type Ctx[T <: Tree]
+
+  sealed trait Tree { override def toString = showTree(this).toString }
+    sealed trait Type extends Tree
+    object Type {
+      final case class Func(from: Ctx[Type], to: Ctx[Type]) extends Type
+      final case class Rec(fields: List[(Ctx[Name], Ctx[Type])]) extends Type
     }
-    sealed trait Term extends Stmt
-      final case class Ascribe(value: Term, typ: Type) extends Term
-      final case class Func(name: Name, typ: Type, value: Term) extends Term
-      final case class Block(stats: List[Stmt]) extends Term
-      final case class New(stats: List[Stmt]) extends Term
-      final case class Apply(fun: Term, arg: Term) extends Term
-      final case class Name(value: String) extends Term with Import.Sel
-      final case class Select(prefix: Term, name: Name) extends Term
+    sealed trait Stmt extends Tree
+      final case class Val(name: Ctx[Name], typ: Ctx[Type], value: Ctx[Term]) extends Stmt
+      final case class Import(t: Ctx[Term], sels: List[Ctx[Import.Sel]]) extends Stmt
+      object Import {
+        sealed trait Sel extends Tree
+        final case class Rename(from: Ctx[Name], to: Ctx[Name]) extends Sel
+        final case class Exclude(name: Ctx[Name]) extends Sel
+        final case object Wildcard extends Sel
+      }
+      sealed trait Term extends Stmt
+        final case class Ascribe(value: Ctx[Term], typ: Ctx[Type]) extends Term
+        final case class Func(name: Ctx[Name], typ: Ctx[Type], value: Ctx[Term]) extends Term
+        final case class Block(stats: List[Ctx[Stmt]]) extends Term
+        final case class New(stats: List[Ctx[Stmt]]) extends Term
+        final case class Apply(fun: Ctx[Term], arg: Ctx[Term]) extends Term
+        final case class Name(value: String) extends Term with Import.Sel
+        final case class Select(prefix: Ctx[Term], name: Ctx[Name]) extends Term
 
-object util {
-  implicit def show[T <: Tree]: Show[T] = Show {
+  implicit def showCtx[T <: Tree]: Show[Ctx[T]]
+  implicit def showTree[T <: Tree]: Show[T] = Show {
     case Type.Func(from, to)    => s(from, " => ", to)
     case Type.Rec(Nil)          => s("{}")
     case Type.Rec(fields)       => s("{ ", r(fields.map { case (n, t) => s("val ", n, ": ", t) }, " "), " }")
@@ -36,7 +42,8 @@ object util {
     case Import.Rename(fr, to)  => s(fr, " => ", to)
     case Import.Exclude(n)      => s(n, " => _")
     case Import.Wildcard        => s("_")
-    case Func(name, typ, value) => s(name, ": ", typ, " => ", value)
+    case Ascribe(term, typ)     => s(term, ": ", typ)
+    case Func(name, typ, value) => s("(", name, ": ", typ, ") => ", value)
     case Block(Nil)             => s("{}")
     case Block(stats)           => s("{ ", r(stats.map(i(_)), " "), n("}"))
     case New(Nil)               => s("new {}")
@@ -44,14 +51,25 @@ object util {
     case Name(value)            => s(value)
     case Select(pre, name)      => s(pre, ".", name)
     case Apply(f, arg)          => s(f, "(", arg, ")")
-    case Ascribe(term, typ)     => s(term, ": ", typ)
   }
-
-  def abort(msg: String): Nothing = throw new Exception(msg)
 }
-import util._
+
+object Untyped extends TreeTemplate {
+  type Ctx[T <: Tree] = T
+  implicit def showCtx[T <: Tree]: Show[Ctx[T]] = showTree
+}
+
+object Typed extends TreeTemplate {
+  sealed trait Ctx[T <: Tree]
+  final case class `:`[T <: Tree](tree: T, typ: Type) extends Ctx[T]
+  final case class No[T <: Tree](tree: T) extends Ctx[T]
+  implicit def showCtx[T <: Tree]: Show[Ctx[T]] = Show { ctx => s(ctx.tree) }
+}
+
+object abort { def apply(msg: String): Nothing = throw new Exception(msg) }
 
 object parse extends StandardTokenParsers {
+
   lexical.delimiters ++= List("(", ")", "{", "}", ":", "=>", ".", "=", ",", ";")
   lexical.reserved   ++= List("new", "val", "import", "macro", "_")
 
@@ -90,41 +108,111 @@ object parse extends StandardTokenParsers {
 }
 
 object typecheck {
-  def apply(stats: List[Stmt]): List[Stmt] = ???
+  import uscala.{Untyped => U, Typed => T}
+  import T.{Ctx, `:`, No}
+
+  type Env = Map[T.Name, T.Type]
+
+  implicit class Subtype(t: T.Type) {
+    def `<:`(other: T.Type) = ???
+  }
+
+  // TODO: validated sels
+  def sels(sels: List[U.Import.Sel])(implicit env: Env = Map.empty): List[Ctx[T.Sel]] = ???
+
+  def stats(stats: List[U.Stmt])(implicit env: Env = Map.empty): List[Ctx[T.Stmt]] = stats match {
+    case Nil => Nil
+    case U.Val(U.Name(n), tpt, body) :: rest =>
+      val tname = T.Name(n) `:` tpt
+      val ttpt  = typ(stats)
+      val tbody = term(body)(env + (tname -> ttpt))
+      // TODO: require tbody `<:` ttpt
+      No(T.Val(tname, ttpt, tbody)) :: stats(rest)
+    case Import(from, sels) :: rest =>
+      val tfrom = term(from)
+      val tsels
+      No(T.Import(tfrom, tsels)) :: stats(rest)
+  }
+
+  def term(term: U.Term)(implicit env: Env = Map.empty): Ctx[T.Term] = ???
 }
 
+/*
 object expand {
   type Env = Map[Name, Term => Term]
 
-  def stats(stats: List[Stmt])(implicit env: Env = Map.empty): List[Stmt] = stats
-  def apply(t: Term)(implicit env: Env = Map.empty): Term = t
+  var i = 0
+  def fresh(pre: String = "x") = { i += 1; Name(pre + "$" + i) }
+
+  def bstats(stats: List[Stmt])(implicit env: Env = Map.empty): List[Stmt] = stats match {
+    case Nil                     => Nil
+    case (t: Term) :: rest       => expand(t) :: bstats(rest)
+    case Val(n, t, b) :: rest    =>
+      val x = fresh(n.value)
+      val newenv = env + (n -> ((_: Term) => x))
+      Val(x, t, expand(b)(newenv)) :: bstats(rest)(newenv)
+    case Import(t, sels) :: rest => Import(expand(t), sels) :: bstats(rest) // TODO: rename this guys too
+  }
+
+  def nstats(stats: List[Stmt])(implicit env: Env = Map.empty): List[Stmt] = stats.map {
+    case t:        => expand(t)
+    case Val(n, t, b)    => Val(n, t, expand(b))
+    case Import(t, sels) => Import(expand(t), sels)
+  }
+
+  def apply(t: Term)(implicit env: Env = Map.empty): Term = t match {
+    case Apply(fun, arg)    => Apply(expand(fun), expand(arg))
+    case Block(stats)       => Block(expand.bstats(stats))
+    case Select(qual, name) => Select(expand(qual), name)
+    case Ascribe(v, t)      => Ascribe(expand(v), t)
+    case New(stats)         => New(expand.nstats(stats))
+    case Func(n, t, b)      => val x = fresh(n.value); Func(x, t, expand(b)(env + (n -> (_ => x))))
+    case n: Name            => env.get(n).map(f => f(n)).getOrElse(n)
+  }
 }
 
 object eval {
   type Env = Map[Name, Term]
 
+  var steps = 0
+  val limit = 100
+
+  def addb(env: Env, binding: (Name, Term)): Env = {
+    val (n, _) = binding
+    if(env.contains(n)) println(s"!!!!!!! entered $n twice")
+    env + binding
+  }
+
+  def addbs(env: Env, bindings: List[(Name, Term)]): Env = {
+    var env1 = env
+    bindings.foreach { b => env1 = addb(env1, b) }
+    env1
+  }
+
   def stats(stats: List[Stmt])(implicit env: Env = Map.empty): List[Stmt] = stats match {
     case Nil => Nil
     case (v @ Val(name, tpt, body)) :: rest =>
-      val ebody = eval(body)
-      Val(name, tpt, ebody) :: eval.stats(rest)(env + (name -> ebody))
+      val ebody = eval(body)(addb(env, name -> body))
+      Val(name, tpt, ebody) :: eval.stats(rest)(addb(env, name -> ebody))
     case Import(t, sels) :: rest =>
       val bindings = sels.map {
         case n: Name                 => n -> Select(t, n)
         case Import.Rename(from, to) => to -> Select(t, from)
         case _                       => ???
       }
-      eval.stats(rest)(env ++ bindings)
+      eval.stats(rest)(addbs(env, bindings))
     case (t: Term) :: rest =>
       eval(t) :: eval.stats(rest)
   }
 
   def apply(term: Term)(implicit env: Env = Map.empty): Term = {
+    steps += 1
+    if (steps > limit) abort("step limit reached")
     val res = term match {
       case Apply(fun, value) =>
         val efun = eval(fun)
         efun match {
-          case Func(name, _, body) => eval(body)(env + (name -> value))
+          case Func(name, _, body) => eval(body)(addb(env, name -> value))
           case _                   => abort("can't apply $value to $efun")
         }
       case Block(stats) =>
@@ -156,7 +244,13 @@ object eval {
 }
 
 object Test extends App {
-  eval.stats(parse("""
-    (new { val x: {} = {} }).x
-  """).get)
+  val parsed = parse("""{
+    val f: {} => {} = (x: {}) => f(x)
+    f({})
+  }""")
+  println(parsed)
+  //eval.stats(parsed.get)
+  val exp = expand.nstats(parsed.get)
+  println(s"expanded: $exp")
+  eval.stats(exp)
 }
